47716260eaf67bc6e5168463d0f7af077437e20b
Goronet <goronet@gmail.com>
2024-05-15 23:43:55 +0200

    Jobs, Tags, TDD, Oh My!

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,11 +1,10 @@
-@props(['size' => 'base'])
+@props(['tag', 'size' => 'base'])
 
 @php
     $classes = 'bg-white/10 hover:bg-white/25 rounded-xl font-bold transition-colors duration-300';
 
     if ($size === 'base') {
         $classes .= ' px-5 py-1 text-sm';
-
     }
 
     if ($size === 'small') {
@@ -13,4 +12,4 @@
     }
 @endphp
 
-<a href="#" class="{{ $classes }}">{{ $slot }}</a>
+<a href="/tags/{{ strtolower($tag->name) }}" class="{{ $classes }}">{{ $tag->name }}</a>
